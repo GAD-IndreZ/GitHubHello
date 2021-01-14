c934ee0c2f4794aed318e0a4c33ac72f43eaf23b
GAD-IndreZ <77437051+GAD-IndreZ@users.noreply.github.com>
2021-01-14 10:26:40 +0000

    Initial commit

---
+++ Assets/Scripts/GithubTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Comment, woop woop
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
